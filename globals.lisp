ea694354e0631fe8fc93accfb4208e1f1d603459
ormf <junk14@selma.hfmdk-frankfurt.de>
2025-02-21 14:17:43 +0100

    misc.

--- globals.lisp
+++ globals.lisp
@@ -165,6 +165,16 @@ zerobased index.")
 
 ;;; (mapcar #'cons *orgel-global-targets* *orgel-global-target-syms*)
 
+(defun target-string->sym (key)
+  (cdr
+   (assoc key
+          '(("level" . level) ("delay" . delay) ("q" . q) ("gain" . gain)
+            ("osc-level" . osc-level) ("base-freq" . base-freq) ("phase" . phase)
+            ("bias-pos" . bias-pos) ("bias-bw" . bias-bw) ("bias-type" . bias-type)
+            ("main" . main) ("min-amp" . min-amp) ("max-amp" . max-amp)
+            ("ramp-up" . ramp-up) ("ramp-down" . ramp-down) ("exp-base" . exp-base))
+          :test #'string=)))
+
 (defun target-key->sym (key)
   (cdr
    (assoc key
